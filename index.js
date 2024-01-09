d8ede8002eda91af6869c502b0f861c14ca75576
Miguel Leonar <migsldev@gmail.com>
2024-01-09 22:42:42 +1100

    5 test passed

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+// customerName
+var customerName = 'bob'
+
+//upperCaseCustomerName
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+//upperCaseCustomerName(); // return BOB
+console.log(customerName);
+
+// setBestCustomer
+
+var bestCustomer
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    //return customerName;
+}
+//setBestCustomer(); // returns not bob
+console.log(bestCustomer);
+
+//overwriteBestCustomer
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+//overwriteBestCustomer(); // returns maybe bob
+console.log(bestCustomer);
+
+//leastFavoriteCustomer
+
+const leastFavoriteCustomer = 'john'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'same old bob';
+}
+changeLeastFavoriteCustomer();
+console.log(leastFavoriteCustomer);
